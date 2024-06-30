c36ba8a668262fea56e8cc27c818610e9c212269
ZhannaKhizrieva <zhanna-apteka@mail.ru>
2024-06-30 14:39:17 +0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+//декларативный
+import { createElement } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//императивный
+export const App = () => {
+	const year = new Date().getFullYear();
+
+	return (
+		//декларативный
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement(
+				'header',
+				{ className: 'App-header' },
+				createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+				createElement(
+					'p',
+					null,
+					'Edit ',
+					createElement('code', null, 'src/App.js'),
+					'and save to reload.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('div', null, year),
+			),
+		)
+	);
+};
